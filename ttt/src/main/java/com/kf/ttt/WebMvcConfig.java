0d96e5a5a61926566004ee5cfab7392799a9b8a7
hyeeebin <knwsome@gmail.com>
2023-09-14 15:49:44 +0900

    engToKorean java 변환 중
    webconfig 설정 추가 - api 경로

--- ttt/src/main/java/com/kf/ttt/WebMvcConfig.java
+++ ttt/src/main/java/com/kf/ttt/WebMvcConfig.java
@@ -1,14 +1,18 @@
 package com.kf.ttt;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer{
 	
+	
+	//리액트 연동 cors 문제
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
@@ -16,5 +20,12 @@ public class WebMvcConfig implements WebMvcConfigurer{
 			.allowedMethods("OPTIONS","GET","POST","PUT","DELETE");   
 		
 	
+	}
+	
+	
+	//url prefix 설정
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("com.kf.ttt"));
 	}
 }
--- ttt/src/main/java/com/kf/ttt/controller/TransController.java
+++ ttt/src/main/java/com/kf/ttt/controller/TransController.java
@@ -1,60 +1,29 @@
 package com.kf.ttt.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kf.ttt.service.TransService;
+
+//http 요청 처리하는 역할
+//클라이언트 요청 받아서 비즈니스 로직 호출한 후, 클라이언트에 다시 반환하는 로직
+
+@RestController
+@RequestMapping("/trans")
 public class TransController {
-	public static void main(String args[]) {
-		System.out.println("aaabbb");
+
+
+	@Autowired
+	TransService transService;
+	
+	@GetMapping("/{text}")
+	public List<String> engToKorean(@PathVariable("text") String text){
+		return transService.engToKorean(text);
+		
 	}
 }
-
-//
-//import re
-//
-//#숫자+단위
-//def changeUnit(text):
-//    nstring = ""
-//    
-//    nlist = list(text.upper())
-//    nd = {'k':'킬로','m':'미터'
-//          
-//          }
-//
-//
-//#영어를 한글로
-//def changeText(text):
-//    
-//    #nstring을 문자열로 초기화
-//    nstring = ""
-//    
-//    # 입력 문자열을 대문자로 변환 후 리스트로 변환
-//    nlist = list(text.upper())  
-//    nd = {
-//        'A': '에이', 'B': '비', 'C': '씨', 'D': '디', 'E': '이', 'F': '에프',
-//        'G': '지', 'H': '에이치', 'I': '아이', 'J': '제이', 'K': '케이', 'L': '엘',
-//        'M': '엠', 'N': '엔', 'O': '오', 'P': '피', 'Q': '큐', 'R': '알',
-//        'S': '에스', 'T': '티', 'U': '유', 'V': '브이', 'W': '더블유', 'X': '엑스',
-//        'Y': '와이', 'Z': '제트'
-//    }
-//    
-//    #입력 문자열의 각 문자를 확인하여 nd 딕셔너리에 해당하는 대체 문자열을 만들어서 nstring에 추가
-//    for i in nlist:
-//        if i in nd:
-//            nstring += nd[i]
-//        else:
-//            nstring += i
-//
-//    return nstring
-//
-//#사용자에게 입력을 받고 변환 결과 출력
-//text = input('입력하세요: ')
-//
-//#입력받은 문자열이 정규식(한글/영어/숫자)에 매치되는지 검사
-//compile_text = re.fullmatch(("^[a-zA-Z가-힣0-9]+$"),text)
-//
-//#match 객체 발생
-//print(compile_text)
-//
-//if compile_text != None:
-//     result = changeText(text)
-//     print("("+text+")/("+result+")") #전사 규칙 형태
-//else:
-//    print("2byte 특수기호, 한자, 일본어 등은 오류로 간주합니다. 다시 입력하세요 ")

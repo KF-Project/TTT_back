52f3a85de63d562ed1290dc18ba1d524c2538bb9
ullallas <chou1520@yonsei.ac.kr>
2023-10-05 15:50:13 +0900

    전화번호 수정 완료 및 소수 오류 수정 중

--- ttt/src/main/java/com/kf/ttt/num_kor/NumberConverter3.java
+++ ttt/src/main/java/com/kf/ttt/num_kor/NumberConverter3.java
@@ -4,9 +4,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 //원투, 하나둘, 일이, 트웰브
-
 
 public class NumberConverter3 {
     private static final String[] korNums = {"공", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"};
@@ -48,27 +46,21 @@ public class NumberConverter3 {
     
     	String input = scanner.nextLine();
     	
+    	//자연수 정규식 패턴
         Pattern pattern = Pattern.compile("\\d+");
         Matcher matcher = pattern.matcher(input);
     	
+        //소수 정규식 패턴 -> 기념일?
     	Pattern p4dec = Pattern.compile("\\d+\\.\\d+");
         Matcher m4dec = p4dec.matcher(input);
         
+        //전화번호 정규식 패턴
         Pattern p4phone = Pattern.compile("\\d+\\ \\d+\\ \\d+");
         Matcher m4phone = p4phone.matcher(input);
         
+        Pattern p4phone_hy = Pattern.compile("\\d+\\-\\d+\\-\\d+");
+        Matcher m4phone_hy = p4phone_hy.matcher(input);
         
-        
-        
-        
-        //띄어쓰기 된 전화번호 출력
-        while (m4phone.find()) {
-        	String number = m4phone.group();
-        	String phoneConversion = convertToKorean(number);
-        	
-        	System.out.println(input.replaceFirst(Pattern.quote(number), "(" + number + ")/(" + phoneConversion + ")"));
-            
-        }
         
         //소수 출력
         while (m4dec.find()) {
@@ -82,7 +74,7 @@ public class NumberConverter3 {
         }
         	
         //소수 외 나머지 출력
-        while (matcher.find()) {
+        if (matcher.find()) {
         	
         	String number = matcher.group();
         	String phoneConversion = convertToKorean(number);
@@ -101,28 +93,38 @@ public class NumberConverter3 {
             	
             }
             
-        	
-            if (number.charAt(0) == '0') {
+            if (number.charAt(0) == '0') { //띄어쓰기 없는 전화번호 출력
             	System.out.println(input.replaceFirst(Pattern.quote(number), "(" + number + ")/(" + phoneConversion + ")"));
-//            	if 
+            	
+            	if (m4phone.find()) { //띄어쓰기 된 전화번호 출력
+            		String num4phone = m4phone.group();
+                	String phoneConversion_sp = convertToKorean(num4phone);
+                	
+                	System.out.println(input.replaceFirst(Pattern.quote(num4phone), "(" + num4phone + ")/(" + phoneConversion_sp + ")"));
+                	
+            	} else if (m4phone_hy.find()) { //하이픈 있는 전화번호 출력
+            		String num4phone = m4phone_hy.group();
+                	String phoneConversion_hy = convertToKorean(num4phone);
+                	
+                	System.out.println(input.replaceFirst(Pattern.quote(num4phone), "(" + num4phone + ")/(" + phoneConversion_hy + ")"));
+            	}
                 
-            } else if (found) {
+            } else if (found) { //날짜 출력
             	System.out.println(input.replaceFirst(Pattern.quote(number + date), "(" + number + date + ")/(" + NumberConverter.numberToWordKo(Integer.parseInt(number), true) + " " + date + ")"));
                 
-            } else if (Integer.parseInt(number) >= 10000 && number.charAt(0) == '1') {
+            } else if (Integer.parseInt(number) >= 10000 && number.charAt(0) == '1') { //10000 이상이고 1로 시작하는 수 앞에 '일' 출력
             	System.out.println(input.replace(matcher.group(), "(" + number + ")/(" + NumberConverter.numberToWordKo2(Integer.parseInt(number), true) + ")"));
             	System.out.println(input.replaceFirst(Pattern.quote(number), "(" + number + ")/(" + NumberConverter.numberToWordKo(Integer.parseInt(number), true) + ")"));
                 
-            } else if (Integer.parseInt(number) <= 99 && Integer.parseInt(number) > 0) {
+            } else if (Integer.parseInt(number) <= 99 && Integer.parseInt(number) > 0) { //'하나, 둘, 셋' 출력
             	System.out.println(input.replace(matcher.group(), "(" + number + ")/(" + NumberConverter.numberToWordKo2(Integer.parseInt(number), true) + ")"));
             	System.out.println(input.replaceFirst(Pattern.quote(number), "(" + number + ")/(" + NumberConverter2.convertToKorean(Integer.parseInt(number)) + ")"));
                 
-            } else {
+            } else { //'일, 이, 삼' 출력
             	System.out.println(input.replaceFirst(Pattern.quote(number), "(" + number + ")/(" + NumberConverter.numberToWordKo(Integer.parseInt(number), true) + ")"));
             }
             
         }
-        
 
         scanner.close();
     }

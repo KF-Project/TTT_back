e69585efe2a5e750250bf070c29f8963094963ed
ullallas <chou1520@yonsei.ac.kr>
2023-10-06 15:51:52 +0900

    소수 전사 오류 수정 완료

--- ttt/src/main/java/com/kf/ttt/num_kor/NumberConverter2.java
+++ ttt/src/main/java/com/kf/ttt/num_kor/NumberConverter2.java
@@ -3,10 +3,10 @@ package com.kf.ttt.num_kor;
 
 //한국어 수사 셈낱씨(기수사)
 public class NumberConverter2 {
-  private static final String[] NUMBER_NAMES = {
+  private static final String[] number_names = {
     "영", "하나", "둘", "셋", "넷", "다섯", "여섯", "일곱", "여덟", "아홉"
   };
-  private static final String[] TEN_NAMES = {
+  private static final String[] ten_names = {
     "", "열", "스물", "서른", "마흔", "쉰", "예순", "일흔", "여든", "아흔"
   };
 
@@ -16,8 +16,8 @@ public class NumberConverter2 {
       int ten = number / 10;
       int one = number % 10;
 
-      String tenName = TEN_NAMES[ten];
-      String oneName = NUMBER_NAMES[one];
+      String tenName = ten_names[ten];
+      String oneName = number_names[one];
 
       if (ten == 0) {
           return oneName;

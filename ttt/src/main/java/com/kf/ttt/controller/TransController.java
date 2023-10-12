bfc07a0886bca73834f9d05379aa487bee347d61
hyeeebin <knwsome@gmail.com>
2023-10-12 15:54:02 +0900

    전사기록 검색 기능 구현 중 검색 조회 건수가 많을 경우 성능이 저하되는 건 수정 중

--- ttt/src/main/java/com/kf/ttt/controller/TransController.java
+++ ttt/src/main/java/com/kf/ttt/controller/TransController.java
@@ -1,15 +1,17 @@
 package com.kf.ttt.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kf.ttt.entity.TransLog;
 import com.kf.ttt.service.TransLogService;
 import com.kf.ttt.service.TransService;
 
@@ -25,30 +27,62 @@ public class TransController {
 
 	@Autowired
 	TransLogService transLogService;
-	
+
 	@GetMapping("/{text}")
-	public Map<String, String> engToKorean(@PathVariable("text") String text) throws Exception{
+	public Map<String, String> engToKorean(@PathVariable("text") String text) throws Exception {
 
 		String trans_result = transService.engToKorean(text);
-		
-		System.out.println("전사 결과: "+trans_result);
-		
-		//로그인한 user 세션 id 가져오기
+
+		System.out.println("전사 결과: " + trans_result);
+
+		// 로그인한 user 세션 id 가져오기
 //		String user_id = (String) session.getAttribute("session_user_id");
 //		System.out.println(user_id);
-		
-		
-		
-		if(trans_result != null && trans_result != "") {
-			//세션 id로 수정해야함.
+
+		if (trans_result != null && trans_result != "") {
+			// 세션 id로 수정해야함.
 			transLogService.addTransResultToLog("kf03", trans_result);
 			System.out.println(transLogService.addTransResultToLog("kf03", trans_result));
 		}
-		
+
 		Map<String, String> response = new HashMap<>();
 		response.put("text", text);
 		response.put("trans_result", trans_result);
 		return response;
 
+	}
+
+	// 전사 기록 검색
+	//Optional은 one result일 경우에 사용해야할듯
+//	@GetMapping("/search/{trans_result}")
+//	public List<TransLog> searchTransLog1(@PathVariable String trans_result) {
+//		// 세션 로그인 아이디
+//		String user_id = "kf03";
+//		return transLogService.searchTransLog(user_id, trans_result);
+//	}
+
+	
+	@GetMapping("/search/{trans_result}")
+	public Map<String, Object> searchTransLog(@PathVariable String trans_result){
+		Map<String, Object> map = new HashMap<>();
+		String user_id = "kf03";
+		List<TransLog> transLog = transLogService.searchTransLog(user_id, trans_result);
+		
+		if(!transLog.isEmpty()) {
+			map.put("result",transLog);
+			map.put("status", "true");
+			map.put("message","success");
+			
+			System.out.println(map);
+			
+			return map;
+		}else {
+			map.put("status", "false");
+			map.put("message","fail");
+			
+			System.out.println(map);
+			
+			return map;
+		}
 	}
 }
